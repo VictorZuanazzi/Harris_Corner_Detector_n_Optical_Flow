7981066ccd50dfc00ad204a366ae57c6b37f0a74
David Speck <david@saduras.de>
2019-03-04 16:29:50 +0100

    PlotCorners: one color for rings and better filename format

--- plot_corners.m
+++ plot_corners.m
@@ -5,10 +5,10 @@ imshow(image)
 hold on
 
 for i = 1:size(row, 2)
-    plot(col(i),row(i), 'o'); 
+    plot(col(i),row(i), 'oy'); 
 end
 
-file_name = './person_toy/'+ name +'.png';
+file_name = sprintf('person_toy/%s.png', name)
 saveas(gcf,file_name);
 corners = gcf;
 hold off
