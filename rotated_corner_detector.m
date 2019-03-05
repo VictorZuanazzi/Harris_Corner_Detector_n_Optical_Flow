0e7a949a12c536ccb79710d62fa3a91348040cd9
David Speck <david@saduras.de>
2019-03-05 11:53:31 +0100

    Question 1.3: Rotation invariance

---
+++ rotated_corner_detector.m
@@ -0,0 +1,25 @@
+function [] = rotated_corner_detector(image, angle, name, w, t)
+% default parameters
+if nargin < 3
+    name = "";
+end
+if nargin < 4
+    w = 5;
+end
+if nargin < 5
+    t = 1e-5;
+end
+
+rot_image = imrotate(image, angle);
+
+% rotate before 
+[H, row, col, Ix, Iy] = harris_corner_detector(rot_image, t, w, name);
+param_name = name + " t=" + num2str(t) + " w=" + num2str(w);
+plot_corners(rot_image, col,row, param_name);
+plot_IxIy(Ix,Iy,name);
+
+% rotate after
+name = name + " rotate after";
+[H, row, col, Ix, Iy] = harris_corner_detector(image, t, w, name);
+param_name = name + " t=" + num2str(t) + " w=" + num2str(w);
+plot_corners(image, col,row, param_name, angle);
