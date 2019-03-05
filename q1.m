1900ecf40d538726fc8f348849165fc7e0bb3dab
David Speck <david@saduras.de>
2019-03-05 11:17:51 +0100

    Question 1.2: find parameters

--- q1.m
+++ q1.m
@@ -1,7 +1,43 @@
-image = imread('./person_toy/00000001.jpg');
+find_params = false;
 
-threshold = [0, 1];
-t = 0;
-w = 5;
+if find_params
+    % try a set of values for window and threshold to determin best fit
+    
+    % person toy
+    image = imread('./person_toy/00000001.jpg');
+    name = 'person toy';
 
-[H, row, col] = harris_corner_detector(image, t, w);
+    for w = 2:1:10
+        for t = 10.^(-1:-1:-10)
+            [H, row, col] = harris_corner_detector(image, t, w, name);
+        end
+    end
+
+    % pingpong
+    image = imread('./pingpong/0000.jpeg');
+    name = 'pingpong';
+
+    for w = 2:1:10
+        for t = 10.^(-1:-1:-10)
+            [H, row, col] = harris_corner_detector(image, t, w, name);
+        end
+    end
+else
+    % hand picked best values for window size and threshold
+    
+    % person toy
+    image = imread('./person_toy/00000001.jpg');
+    name = "person toy";
+
+    w = 5;
+    t = 1e-5;
+    [H, row, col] = harris_corner_detector(image, t, w, name);
+
+    % ping pong
+    image = imread('./pingpong/0000.jpeg');
+    name = "pingpong";
+
+    w = 5;
+    t = 1e-2;
+    [H, row, col] = harris_corner_detector(image, t, w, name);
+end
